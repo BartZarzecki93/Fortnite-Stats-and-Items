b5f97ff7cdc3b6a27dc20c26ec6f536555af58b3
Bartosz Zarzecki <bartoszzarzecki@Bartoszs-MacBook-Pro.local>
2019-05-05 12:19:59 -0400

    outfit look

--- client/src/components/outfitItem.js
+++ client/src/components/outfitItem.js
@@ -5,37 +5,45 @@ export default function outfitItem({
   Outfit: { identifier, name, description, type, cost, rarity }
 }) {
   return (
-    <div
-      class='card mb-3'
-      style={{
-        width: 350,
-        height: '100%',
-        display: 'block',
-        alignItems: 'center'
-      }}
-    >
-      <h3 class='card-header text-center'>{name}</h3>
-      <br />
-      <img
-        class='mx-auto d-block'
-        style={{
-          width: 180,
-          height: 150
-        }}
-        src={logo}
-        alt='Logo'
-      />
-      <div class='card-body'>
-        <h5 class='card-title text-center'>{description}</h5>
+    <div class='row'>
+      <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>
+        <div class='card-columns'>
+          <div
+            class='card-block mb-3'
+            style={{
+              width: '350',
+              height: '100%',
+              display: 'block',
+              alignItems: 'center'
+            }}
+          >
+            <h3 class='card-header text-center'>{name}</h3>
+            <br />
+            <img
+              class='mx-auto d-block'
+              style={{
+                width: 180,
+                height: 150
+              }}
+              src={logo}
+              alt='Logo'
+            />
+            <div class='card-body'>
+              <h5 class='card-title text-center'>{description}</h5>
 
-        <ul class='list-group list-group-flush'>
-          <li class='list-group-item'>Type: {type}</li>
-          <li class='list-group-item'>Cost: {cost}</li>
-          <li class='list-group-item'>Rarity: {rarity}</li>
-        </ul>
-        <br />
-        <button className='btn btn-primary mx-auto d-block'>Details</button>
-        <br />
+              <ul class='list-group list-group-flush'>
+                <li class='list-group-item'>Type: {type}</li>
+                <li class='list-group-item'>Cost: {cost}</li>
+                <li class='list-group-item'>Rarity: {rarity}</li>
+              </ul>
+              <br />
+              <button className='btn btn-primary mx-auto d-block'>
+                Details
+              </button>
+              <br />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
